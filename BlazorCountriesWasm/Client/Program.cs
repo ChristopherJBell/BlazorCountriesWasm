faf6cc1bf7cb435c2bc97dda6c27515a416aeb04
Christopher Bell <christopherbell@outlook.com>
2022-08-22 10:41:17 +0100

    After Adding Syncfusion

--- BlazorCountriesWasm/Client/Program.cs
+++ BlazorCountriesWasm/Client/Program.cs
@@ -3,6 +3,7 @@ global using BlazorCountriesWasm.Shared;
 using BlazorCountriesWasm.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ICountryService, CountryService>();
+
+builder.Services.AddSyncfusionBlazor();
+
+var SyncfusionLicenceKey = builder.Configuration["SyncfusionLicenceKey"];
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(SyncfusionLicenceKey);
 
 await builder.Build().RunAsync();
